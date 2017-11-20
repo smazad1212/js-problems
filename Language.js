40b88fc3e720ec3e2b96fa8f7471694e705c50e0
Kazi Fozle Azim Rabi <fozleazimrabi2@gmail.com>
2017-11-20 12:14:23 +0600

    max count array solved

--- Language.js
+++ Language.js
@@ -9,4 +9,24 @@ function ucount(arr) {
   return arrCount.length;
 }
 
+function mcount(arr) {
+  let maxElement = '';
+  let maxOccurence = 0;
+  arr.map((element) => {
+    let occurence = 0;
+    arr.map((duplicate) => {
+      if (element == duplicate) {
+        ++occurence;
+      }
+    })
+    if (occurence > maxOccurence) {
+      maxOccurence = occurence;
+      maxElement = element;
+    }
+  })
+  return maxElement;
+}
+
+
 ucount([1, 1, 2, '', 4, 6, 'hello', 7, 8, 'toString', 8, 8, 'world']);
+mcount([1, 1, 2, '', 4, 6, 'hello', 7, 8, 'toString', 8, 8, 'world']);
